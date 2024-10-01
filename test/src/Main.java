32b01daab91c2a63151f8161e70324bc8dc812c4
tarikskender <tarikskender23@gmail.com>
2024-10-01 13:37:11 +0200

    Testing Git With Java

---
+++ test/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String name = "Tarik";
+        printOut(name);
+    }
+    public static void printOut(String word){
+        System.out.println(word);
+    }
+}
